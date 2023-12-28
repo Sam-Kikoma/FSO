f09dc85cd7d378a34ccd12ce3c93895796e97c4c
Sam-Kikoma <kikomasamson@gmail.com>
2023-12-28 19:39:32 +0100

    Playing around with MongoDB

---
+++ part3/nodePrac/mongo.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+if (process.argv.length < 3) {
+	console.log("Give password as an argument");
+	process.exit(1);
+}
+
+const password = process.argv[2];
+const url = `mongodb+srv://SamKikoma:${password}@phonebookdb.mcgu6g5.mongodb.net/noteApp?retryWrites=true&w=majority`;
+
+mongoose.set("strictQuery", false);
+mongoose.connect(url);
+
+const noteSchema = new mongoose.Schema({
+	content: String,
+	important: Boolean,
+});
+
+const Note = mongoose.model("Note", noteSchema);
+
+const note = new Note({
+	content: "The boy is cooking",
+	important: true,
+});
+
+//Fetching a note
+Note.find({}).then((result) => {
+	result.forEach((note) => {
+		console.log(note);
+	});
+	mongoose.connection.close();
+});
+//Saving a note
+// note.save().then((result) => {
+// 	console.log("Note saved");
+// 	mongoose.connection.close();
+// });
