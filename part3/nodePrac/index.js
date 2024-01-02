1be5b129d22fe622dfcedcdb9949510f00b6c803
Sam-Kikoma <kikomasamson@gmail.com>
2024-01-02 13:23:04 +0100

    Added validation

--- part3/nodePrac/index.js
+++ part3/nodePrac/index.js
@@ -18,11 +18,12 @@ const errorHandler = (error, request, response, next) => {
 
 	if (error.name === "CastError") {
 		return response.status(400).send({ error: "malformatted id" });
+	} else if (error.name === "ValidationError") {
+		return response.status(400).json({ error: error.message });
 	}
 
 	next(error);
 };
-
 const unknownEndpoint = (request, response) => {
 	response.status(404).send({ error: "unknown endpoint" });
 };
@@ -76,14 +77,14 @@ app.delete("/api/notes/:id", (request, response, next) => {
 });
 
 app.put("/api/notes/:id", (request, response, next) => {
-	const body = request.body;
+	const { content, important } = request.body;
 
-	const note = {
-		content: body.content,
-		important: body.important,
-	};
+	Note.findByIdAndUpdate(
+		request.params.id,
 
-	Note.findByIdAndUpdate(request.params.id, note, { new: true })
+		{ content, important },
+		{ new: true, runValidators: true, context: "query" }
+	)
 		.then((updatedNote) => {
 			response.json(updatedNote);
 		})
