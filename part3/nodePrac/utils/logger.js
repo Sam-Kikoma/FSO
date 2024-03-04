68f04240f45bc0005f8fae02c2620c821dcd59dc
Sam-Kikoma <kikomasamson@gmail.com>
2024-03-04 21:02:18 +0100

    More testing

--- part3/nodePrac/utils/logger.js
+++ part3/nodePrac/utils/logger.js
@@ -1,6 +1,17 @@
-const info = (...params) => console.log(...params);
-const error = (...params) => console.error(...params);
+/* eslint-disable indent */
+const info = (...params) => {
+	if (process.env.NODE_ENV !== "test") {
+		console.log(...params);
+	}
+};
+
+const error = (...params) => {
+	if (process.env.NODE_ENV !== "test") {
+		console.error(...params);
+	}
+};
+
 module.exports = {
-    info,
-    error,
+	info,
+	error,
 };
