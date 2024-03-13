0480f01fc9cb3314b58d4a34fd0333b5979e8dc3
Sam-Kikoma <kikomasamson@gmail.com>
2024-03-13 09:14:24 +0100

    Part 3 testing

--- part3/nodePrac/app.js
+++ part3/nodePrac/app.js
@@ -2,6 +2,7 @@ const config = require("./utils/config");
 const express = require("express");
 const app = express();
 const cors = require("cors");
+require("express-async-errors");
 const notesRouter = require("./controllers/notes");
 const middleware = require("./utils/middleware");
 const logger = require("./utils/logger");
@@ -12,13 +13,13 @@ mongoose.set("strictQuery", false);
 logger.info("connecting to", config.MONGODB_URI);
 
 mongoose
-    .connect(config.MONGODB_URI)
-    .then(() => {
-        logger.info("connected to MongoDB");
-    })
-    .catch((error) => {
-        logger.error("error connecting to MongoDB:", error.message);
-    });
+	.connect(config.MONGODB_URI)
+	.then(() => {
+		logger.info("connected to MongoDB");
+	})
+	.catch((error) => {
+		logger.error("error connecting to MongoDB:", error.message);
+	});
 
 app.use(cors());
 app.use(express.static("dist"));
